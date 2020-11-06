5f6f342740fbf9bbe616d0ee88bf08a90d1bbe17
ygorrc <32312761+ygorrc@users.noreply.github.com>
2020-11-05 23:05:56 -0300

    att de backend
    inclusão da bancoo de dados

--- tecweb.js
+++ tecweb.js
@@ -1,16 +1,42 @@
 var express = require ('express');
 var app = express();
 
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: "Ufsj2010!!",
+    database: "patinetebase"
+});
 app.use(express.json());
 
 app.post("/patinete",(req, resp)=>{
     var patinete = req.body;
-    console.log(JSON.stringify(patinete));
+    console.log("POST - patinete");
+    connection.query("INSERT INTO patinete SET ?",[patinete],(err, result) =>{
+        if(err){
+            console.log(err);
+            resp.status(500).end();
+        }else{
+            resp.status(200);
+            resp.json(result);
+        }
+    });
+
 });
 
 app.get("/patinete/:patineteId",(req,resp) =>{
     var patineteId = req.params.patineteId;
-    console.log("Get - patineteID" +  patineteId );    
+    console.log("GET - patineteID" +  patineteId ); 
+    connection.query("SELECT * FROM patinete WHERE id = ?",[patineteId],(err, result) =>{
+        if(err){
+            console.log(err);
+            resp.status(500).end();
+        }else{
+            resp.status(200);
+            resp.json(result);
+        }
+    });  
 });
 
 app.put("/patinete/:patineteId",(req,resp) =>{
